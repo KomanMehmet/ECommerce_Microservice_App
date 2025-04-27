5fce63d78f2554652ffe164dbc99638fd1ead80f
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-04-27 16:59:44 +0300

    (order-service): Add webAPI for Ordering entity

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var value = await _mediator.Send(new GetOrderingByIdQuery(id));
+
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand createOrderingCommand)
+        {
+            await _mediator.Send(createOrderingCommand);
+
+            return Ok("Ordering created successfully.");
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand updateOrderingCommand)
+        {
+            await _mediator.Send(updateOrderingCommand);
+
+            return Ok("Ordering updated successfully.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(id));
+
+            return Ok("Ordering removed successfully.");
+        }
+}
