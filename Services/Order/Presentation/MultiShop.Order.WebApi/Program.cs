b335bc8bde27ead6c4ae0ccab8463bf9fa43dca2
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-04-27 17:07:53 +0300

    (order-service): Add MediatR service registration

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,9 +1,18 @@
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 //Yeni eklenenler
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -15,6 +24,7 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
 //Yeni eklenenler
 
 builder.Services.AddControllers();
