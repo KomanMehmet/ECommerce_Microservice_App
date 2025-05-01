291a46cc98a25fa7111e919aae64e66c45729eeb
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-01 16:50:54 +0300

    Enforce global authentication using AuthorizeFilter

--- Services/Basket/MultiShop.Basket/Program.cs
+++ Services/Basket/MultiShop.Basket/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using MultiShop.Basket.LoginServices;
 using MultiShop.Basket.Services;
@@ -7,6 +9,8 @@ using MultiShop.Basket.Settings;
 var builder = WebApplication.CreateBuilder(args);
 
 //Yeni Eklenenler
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
 //JWT ile ilgili
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
@@ -29,7 +33,10 @@ builder.Services.AddSingleton<RedisService>(sp =>
 });
 //Yeni Eklenenler
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));//yeni eklendi
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -44,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();//yeni eklendi.
 
 app.UseAuthorization();
 
