59acd13ccf7faed52798c027eae18f50456971ff
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-06 17:33:52 +0300

    add features slider dto to web ui layer

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/FeatureSliderDtos/CreateFeatureSliderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos
+{
+    public class CreateFeatureSliderDto
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
