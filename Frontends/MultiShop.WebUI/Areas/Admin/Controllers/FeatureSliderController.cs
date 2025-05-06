25d6ace71934ce80b932233f9ed30118c307f9c3
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-06 17:56:21 +0300

    (admin): implement FeatureSlider listing in UI and add FeatureSliderController

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/FeatureSliderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/FeatureSlider")]
+    [AllowAnonymous]
+    public class FeatureSliderController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureSliderController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v0 = "Öne Çıkan İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkan Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMesage = await client.GetAsync("https://localhost:7227/api/FeatureSliders");
+
+            if (responseMesage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMesage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
