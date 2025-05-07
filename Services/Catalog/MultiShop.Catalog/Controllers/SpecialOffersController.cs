fedc72ad544bf22f662af5a30bdd13f2c4643083
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-07 16:25:25 +0300

    (catalog-api): implement SpecialOffer entity, DTOs, services, settings, and API controller

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.SpecialOfferDtos;
+using MultiShop.Catalog.Services.SpecialOfferServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var result = await _specialOfferService.GetAllSpecialOfferAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialOfferById(string id)
+        {
+            var result = await _specialOfferService.GetByIdSpecialOfferAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDto);
+
+            return Ok("Special offer created successfully.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDto);
+
+            return Ok("Special offer updated successfully.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id);
+
+            return Ok("Special offer deleted successfully.");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/SpecialOfferDtos/GetByIdSpecialOfferDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Dtos.SpecialOfferDtos
+{
+    public class GetByIdSpecialOfferDto
+    {
+        public string SpecialOfferID { get; set; }
+
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
