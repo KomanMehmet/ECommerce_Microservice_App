5c59087e2b342d7c874eb95e5000a7f7ee91e390
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-07 18:42:36 +0300

    Implemented Feature functionality (DTOs, API controller, and CRUD operations)

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeaturesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.FeatureDtos;
+using MultiShop.Catalog.Services.FeatureServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+
+        public FeaturesController(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FeatureList()
+        {
+            var result = await _featureService.GetAllFeatureAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeatureById(string id)
+        {
+            var result = await _featureService.GetByIdFeatureAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            await _featureService.CreateFeatureAsync(createFeatureDto);
+
+            return Ok("Feature created successfully.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            await _featureService.UpdateFeatureAsync(updateFeatureDto);
+
+            return Ok("Feature updated successfully.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFeature(string id)
+        {
+            await _featureService.DeleteFeatureAsync(id);
+
+            return Ok("Feature deleted successfully.");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/FeatureServices/FeatureService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.FeatureDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.FeatureServices
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly IMongoCollection<Feature> _featureCollection;
+        private readonly IMapper _mapper;
+
+        public FeatureService(IMapper mapper, IDatabaseSettings databasesettings)
+        {
+            var client = new MongoClient(databasesettings.ConnectionString);
+            var database = client.GetDatabase(databasesettings.DatabaseName);
+            _featureCollection = database.GetCollection<Feature>(databasesettings.FeatureCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            var values = _mapper.Map<Feature>(createFeatureDto);
+
+            await _featureCollection.InsertOneAsync(values);
+        }
+
+        public async Task DeleteFeatureAsync(string id)
+        {
+            await _featureCollection.DeleteOneAsync(x => x.FeatureID == id);
+        }
+
+        public async Task<List<ResultFeatureDto>> GetAllFeatureAsync()
+        {
+            var values = await _featureCollection.Find(x => true).ToListAsync();
+
+            return _mapper.Map<List<ResultFeatureDto>>(values);
+        }
+
+        public async Task<GetByIdFeatureDto> GetByIdFeatureAsync(string id)
+        {
+            var value = await _featureCollection.Find<Feature>(x => x.FeatureID == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<GetByIdFeatureDto>(value);
+        }
+
+        public async Task UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDto);
+
+            await _featureCollection.FindOneAndReplaceAsync(x => x.FeatureID == updateFeatureDto.FeatureID, value);
+        }
+    }
+}
