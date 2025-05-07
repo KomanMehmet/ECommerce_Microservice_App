1882c19b905d08e76a1767239fbae15984df7c63
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-07 23:47:51 +0300

    Implement Feature CRUD operations in Admin UI with views

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/FeatureController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Feature")]
+    public class FeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v0 = "Özellik İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Özellikler";
+            ViewBag.v3 = "Özellikler Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7227/api/Features");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateFeature")]
+        public async Task<IActionResult> CreateFeature()
+        {
+            ViewBag.v0 = "Özellik İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Özellikler";
+            ViewBag.v3 = "Yeni Özellik Girişi";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateFeature")]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("https://localhost:7227/api/Features", stringContent);
+
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateFeature/{id}")]
+        public async Task<IActionResult> UpdateFeature(string id)
+        {
+            ViewBag.v0 = "Özellik İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Özellikler";
+            ViewBag.v3 = "Özellik Güncelleme İşlemi";
+
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7227/api/Features/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var value = JsonConvert.DeserializeObject<UpdateFeatureDto>(jsonData);
+
+                return View(value);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateFeature/{id}")]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync("https://localhost:7227/api/Features", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new {ares = "Admin"});
+            }
+
+            return View();
+        }
+
+        [Route("DeleteFeature/{id}")]
+        public async Task<IActionResult> DeleteFeature(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.DeleteAsync("https://localhost:7227/api/Features?id=" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+
+            return View();
+        }
+    }
+}
