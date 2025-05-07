343a74cbaedf935e1848613b6cd89c5a4e9ceb27
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-07 23:16:38 +0300

    Add DTOs for Feature entity in UI layer

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/FeatureDtos/ResultFeatureDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MultiShop.DtoLayer.CatalogDtos.FeatureDtos
+{
+    public class ResultFeatureDto
+    {
+        public string FeatureID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/FeatureDtos/UpdateFeatureDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.FeatureDtos
+{
+    public class UpdateFeatureDto
+    {
+        public string FeatureID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
