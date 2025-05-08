16cb284497178630028ca5eb6b9426b8c873e37f
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-08 16:48:14 +0300

    Implement Feature product CRUD operations in Admin UI with views

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -10,5 +10,7 @@ namespace MultiShop.Catalog.Entities
         public string CategoryID { get; set; }
 
         public string CategoryName { get; set; }
+
+        public string ImageURL { get; set; }
     }
 }
