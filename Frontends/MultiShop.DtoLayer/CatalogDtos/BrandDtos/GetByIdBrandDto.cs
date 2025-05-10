4865722a6690a37b7db76c458797043e79eccc97
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-10 17:25:00 +0300

    Implement Brand CRUD operations in Admin UI with views

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/BrandDtos/GetByIdBrandDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.BrandDtos
+{
+    public class GetByIdBrandDto
+    {
+        public string BrandID { get; set; }
+
+        public string BrandName { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/BrandDtos/ResultBrandDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.BrandDtos
+{
+    public class ResultBrandDto
+    {
+        public string BrandID { get; set; }
+
+        public string BrandName { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
