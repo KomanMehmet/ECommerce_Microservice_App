bd23857ba9820046ab20d233561db6bbeea8c274
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-10 16:19:53 +0300

    Implement OfferDiscount CRUD operations in Admin UI with views

---
+++ Frontends/MultiShop.DtoLayer/CatalogDtos/OfferDiscountDtos/GeyByIdOfferDiscountDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.DtoLayer.CatalogDtos.OfferDiscountDtos
+{
+    public class GeyByIdOfferDiscountDto
+    {
+        public string OfferDiscountID { get; set; }
+
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string ButtonTitle { get; set; }
+    }
+}
