2aa9d56d226245d69b77b79bdd4b38679e81f8ac
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-11 17:05:01 +0300

    Implemented product listing by CategoryID and integrated it into the UI

--- Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
 {
     public class _ProductListComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductListComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var resposnseMessage = await client.GetAsync("https://localhost:7227/api/Products/GetProductWithCategoryByCategoryId?categoryId=" + id);
+
+            if (resposnseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resposnseMessage.Content.ReadAsStringAsync();
+
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+
+                return View(values);
+            }
+
             return View();
         }
     }
