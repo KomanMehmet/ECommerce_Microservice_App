c6b72d3c652cb375be870255f762115eb8f37bde
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-23 12:02:56 +0300

    Create comment microservice with MSSQL, DbContext, and API endpoints

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;initial Catalog=MultiShopCommentDb;user=sa;password=168856.Mn;TrustServerCertificate=True");
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
