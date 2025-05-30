8af75a26f5815714860a2cb7f9dcc9a19d116aff
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-05-30 16:53:28 +0300

    Update launch settings to use HTTP instead of HTTPS and configure program.cs

--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -1,5 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Yeni  eklenenler
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+
+builder.Services.AddOcelot(configuration);
+
 var app = builder.Build();
+
+await app.UseOcelot();
+//Yeni eklenenler
 
 app.MapGet("/", () => "Hello World!");
 
